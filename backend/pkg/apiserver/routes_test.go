ff7035441b650b499d7f7a3c38b20890ef170375
HaRo87 <robert@fam-hansel.de>
2021-01-02 11:44:35 +0100

    increasing test coverage

--- backend/pkg/apiserver/routes_test.go
+++ backend/pkg/apiserver/routes_test.go
@@ -3,7 +3,9 @@ package apiserver
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/genjidb/genji"
+	"github.com/haro87/dokerb/pkg/datastore"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
@@ -16,6 +18,23 @@ type apiError struct {
 	Reason  string `json:"reason"`
 }
 
+type mock struct {
+	Method      string
+	Call        string
+	ReturnValue interface{}
+}
+
+type testCase struct {
+	Description          string
+	Route                string
+	Method               string
+	ExpectedError        bool
+	ExpectedErrorMessage string
+	ExpectedCode         int
+	Mock                 []mock
+}
+
+var m *datastore.MockGenjiDB
 var db *genji.DB
 var td string
 
@@ -31,65 +50,127 @@ func setupTestCaseForRealDB(t *testing.T) func(t *testing.T) {
 	}
 }
 
-func TestSessionAPIRoutes(t *testing.T) {
-	setupAndTearDown := setupTestCaseForRealDB(t)
+func setupTestCaseForMock(t *testing.T) func(t *testing.T) {
+	m = new(datastore.MockGenjiDB)
+	return func(t *testing.T) {
+		m = nil
+	}
+}
+
+func TestAPIRoutesForErrors(t *testing.T) {
+	setupAndTearDown := setupTestCaseForMock(t)
 	defer setupAndTearDown(t)
 
-	testCases := []struct {
-		description          string
-		route                string
-		method               string
-		expectedError        bool
-		expectedErrorMessage string
-		expectedCode         int
-	}{
+	testCases := []testCase{
 		{
-			description:          "Create a new Session succeeds",
-			route:                "/api/sessions",
-			method:               "POST",
-			expectedError:        false,
-			expectedErrorMessage: "",
-			expectedCode:         200,
+			Description:          "Create a new Session fails",
+			Route:                "/api/sessions",
+			Method:               "POST",
+			ExpectedError:        false,
+			ExpectedErrorMessage: "Unable to store session token",
+			ExpectedCode:         500,
+			Mock: []mock{
+				mock{
+					Method:      "Exec",
+					Call:        "INSERT INTO sessions VALUES ?",
+					ReturnValue: fmt.Errorf("Ooops, something went wrong"),
+				},
+			},
 		},
 		{
-			description:          "Deleting a sessions fails due to wrong token length",
-			route:                "/api/sessions/12345",
-			method:               "DELETE",
-			expectedError:        false,
-			expectedErrorMessage: "Session token does not match desired length",
-			expectedCode:         500,
+			Description:          "Deleting a sessions fails due to wrong token length",
+			Route:                "/api/sessions/12345",
+			Method:               "DELETE",
+			ExpectedError:        false,
+			ExpectedErrorMessage: "Session token does not match desired length",
+			ExpectedCode:         500,
 		},
+		{
+			Description:          "Adding a user to a session fails due to wrong token length",
+			Route:                "/api/sessions/12345/users/Tigger",
+			Method:               "POST",
+			ExpectedError:        false,
+			ExpectedErrorMessage: "Session token does not match desired length",
+			ExpectedCode:         500,
+		},
+		{
+			Description:          "Getting users fails due to wrong token length",
+			Route:                "/api/sessions/12345/users",
+			Method:               "GET",
+			ExpectedError:        false,
+			ExpectedErrorMessage: "Session token does not match desired length",
+			ExpectedCode:         500,
+		},
+		{
+			Description:          "Deleting a user fails due to wrong token length",
+			Route:                "/api/sessions/12345/users/Tigger",
+			Method:               "DELETE",
+			ExpectedError:        false,
+			ExpectedErrorMessage: "Session token does not match desired length",
+			ExpectedCode:         500,
+		},
+		{
+			Description:          "Getting work packages fails due to wrong token length",
+			Route:                "/api/sessions/12345/workpackages",
+			Method:               "GET",
+			ExpectedError:        false,
+			ExpectedErrorMessage: "Session token does not match desired length",
+			ExpectedCode:         500,
+		},
+		// {
+		// 	Description:          "Adding a work package to a session fails due to wrong token length",
+		// 	Route:                "/api/sessions/12345/workpackages/TEST01",
+		// 	Method:               "POST",
+		// 	ExpectedError:        false,
+		// 	ExpectedErrorMessage: "Session token does not match desired length",
+		// 	ExpectedCode:         500,
+		// },
+		// {
+		// 	Description:          "Deleting a work package fails due to wrong token length",
+		// 	Route:                "/api/sessions/12345/workpackages/TEST01",
+		// 	Method:               "DELETE",
+		// 	ExpectedError:        false,
+		// 	ExpectedErrorMessage: "Session token does not match desired length",
+		// 	ExpectedCode:         500,
+		// },
 	}
 
+	m.On("Exec", "CREATE TABLE sessions").Return(nil)
+
 	app := NewServer(&Config{
-		Database: database{Location: td + "/my.db"},
-		Static:   static{Prefix: "/public", Path: "../../static"},
-	}, db).Start()
+		Static: static{Prefix: "/public", Path: "../../static"},
+	}, m).Start()
 
 	for _, test := range testCases {
 		req, _ := http.NewRequest(
-			test.method,
-			test.route,
+			test.Method,
+			test.Route,
 			nil,
 		)
 
-		res, err := app.Test(req, -1)
-
-		assert.Equalf(t, test.expectedError, err != nil, test.description)
-
-		if test.expectedErrorMessage != "" {
-			var m apiError
-			decoder := json.NewDecoder(res.Body)
-			derr := decoder.Decode(&m)
-			assert.NoError(t, derr)
-			assert.Equal(t, "error", m.Message)
-			assert.Equal(t, test.expectedErrorMessage, m.Reason)
+		if len(test.Mock) > 0 {
+			for _, moc := range test.Mock {
+				m.On(moc.Method, moc.Call).Return(moc.ReturnValue)
+			}
 		}
 
-		if test.expectedError {
+		res, err := app.Test(req, -1)
+
+		assert.Equalf(t, test.ExpectedError, err != nil, test.Description)
+
+		if test.ExpectedErrorMessage != "" {
+			var ae apiError
+			decoder := json.NewDecoder(res.Body)
+			derr := decoder.Decode(&ae)
+			assert.NoError(t, derr)
+			assert.Equal(t, "error", ae.Message)
+			assert.Equal(t, test.ExpectedErrorMessage, ae.Reason)
+		}
+
+		if test.ExpectedError {
 			continue
 		}
 
-		assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
+		assert.Equalf(t, test.ExpectedCode, res.StatusCode, test.Description)
 	}
 }
